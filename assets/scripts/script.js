817f883aa7a1f5305abbc758177fd226896a169f
Cailin Bell Wold <79965490+CailinBellWold@users.noreply.github.com>
2021-04-06 10:50:15 -0700

    First function validates.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,13 +1,15 @@
 // Assignment Code
 
-//Set up variables for everything we will ask the user about their password preferences.
-var askLength;
-var askLowercase;
-var askUppercase;
-var askNumeric;
-var askSpecial;
+//Variables for all preferences.
+let askLength;
+let askLowercase;
+let askUppercase;
+let askNumeric;
+let askSpecial;
+let securePassword;
+let userInput;
 
-//Set up arrays for potential answers
+//Arrays for answers (outside of null or empty)
 //Do we have to set up every number from 8-128? Look for how to do this.
 /*Clipped from GH: const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 const letter = letters[0]; // "A"
@@ -20,29 +22,34 @@ lowercase = ["abcdefghijklmnopqrstuvwxyz"]
 uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
 numeric = ["1234567890"]
 special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+securePassword = "writePassword function output"
 
 //querySelector() returns the first Element within the document that matches the specified selector, or group of selectors. If no matches are found, null is returned.
 var generateBtn = document.querySelector("#generate"); 
 
-// Add event listener to generate button
-//generateBtn.addEventListener("click", function() { prompt("Please select a password length between 8 and 128 characters.")}); //Works
+//When the button is clicked, start the process that will put the password in the box. The result of writePassword (when complete...(I keep getting all the steps displaying. Make a v for the final password.)), should appear there. 
+generateBtn.addEventListener("click", writePassword); {
+  document.getElementById("password").placeholder = securePassword;
+};
 
-generateBtn.addEventListener("click", function writePassword() { prompt("Please select a password length between 8 and 128 characters.", [8])});
-
-/*----Cuttoff. If we comment out here down, pop-up will still work */
+//----Cuttoff. If we comment out here down, pop-up will still work
 function writePassword() {
-  if (USER INPUT > 8 && USER INPUT < 129){
-      alert(woot);
-  } else {
-    alert("Please enter a whole number between 8 and 128.")
-  }
+  userInput = prompt("Please select a password length between 8 and 128 characters.", [8]);
+    if (!userInput || userInput <= 7 || userInput >= 129) {
+      alert("Please enter a whole number between 8 and 128.");
+    } else {
+      alert("This worked.");
+    };
+  };
+/*
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-}
+};
+
 // Write password to the #password input (THIS IS THE ORIGINAL. KEEP FOR REFERENCE.)
 /*function writePassword() {
   var password = generatePassword();
